4d96520dbbcbd49ce29d7bd951baac07b2676fef
kennedyoponyo <125536491+kennedyoponyo@users.noreply.github.com>
2023-03-23 07:37:44 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,4 +13,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+int convert_day(int month, int day); 
